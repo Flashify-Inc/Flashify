19cec732c2abadfc064bd3c6dfd06b2e6485ca28
YunoBLANK <y.khayour@aui.ma>
2023-04-27 15:09:37 +0200

    Updated the categories values to one arraylist. replaced the flag technique with a clicklisteners accross the activities

--- app/src/main/java/com/example/flashify/MainActivity.java
+++ app/src/main/java/com/example/flashify/MainActivity.java
@@ -10,10 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton addBtn, manualBtn, magicBtn;
     Button categoryBtn1, categoryBtn2;
-    int flag1;
+
 
 
     @Override
@@ -42,47 +44,60 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+        /***************************     INITIALIZATION     ******************************/
 
-        Flashcard f1 = new Flashcard("what is the capital of Spain ?", "Madrid");
-        Flashcard f2 = new Flashcard ("What is the capital of Italy ?", "Rome");
-        Flashcard f3 = new Flashcard ("H2O","the water chemical compound");
-        Flashcard f4 = new Flashcard ("dilution","the act of decreasing the concentration of a soluble");
+                // DATABASE DUMMY DATA
+                ArrayList<Category> database = new ArrayList<Category>();
+                database.add(new Category("Geography" ));
+                database.add(new Category("Chemistry" ));
+                database.get(0).getFlashcards().add(new Flashcard("what is the capital of Spain ?", "Madrid"));
+                database.get(0).getFlashcards().add(new Flashcard ("What is the capital of Italy ?", "Rome"));
+                database.get(1).getFlashcards().add(new Flashcard ("H2O","the water chemical compound"));
+                database.get(1).getFlashcards().add(new Flashcard ("dilution","the act of decreasing the concentration of a soluble"));
 
-        Category c1 = new Category("Geography" );
-        c1.getFlashcards().add(f1);
-        c1.getFlashcards().add(f2);
 
-        Category c2 = new Category("Chemistry");
-        c2.getFlashcards().add(f3);
-        c2.getFlashcards().add(f4);
+                // Category initialization
+                ArrayList<Category> categories = new ArrayList<Category>();
+                String TempN, TempF, TempB;
 
+                for(int id = 0; id< database.size() ; id++) {
+                    // loading category from the database
+                    TempN = database.get(id).getName();
+                    categories.add(new Category(TempN));
+
+                    for (int idf = 0; idf< database.get(id).getFlashcards().size() ; idf++) {
+                        // loading flashcards from the database
+                        TempF = database.get(id).getFlashcards().get(idf).getFront();
+                        TempB = database.get(id).getFlashcards().get(idf).getBack();
+                        categories.get(id).getFlashcards().add(new Flashcard(TempF, TempB) );
+                    }
+                }
+
+        /*****  MAKE A LIST OF BUTTONS TO (setNAME & setVISIBLITY) EACH TIME A CATEGORY IS CREATED  *****/
         categoryBtn1 = findViewById(R.id.btnCategory1);
         categoryBtn2 = findViewById(R.id.btnCategory2);
 
+        categoryBtn1.setText(categories.get(0).getName());
+        categoryBtn2.setText(categories.get(1).getName());
 
-        categoryBtn1.setText(c1.getName());
-        categoryBtn2.setText(c2.getName());
-
+        //first category
         categoryBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intentC = new Intent (MainActivity.this,CategoryView.class);
-                intentC.putExtra("co1", c1);
-                flag1 = 1;
-                intentC.putExtra("flag1", flag1);
+                intentC.putExtra("co", categories.get(0));
                 startActivity(intentC);
             }
         });
 
+        //second category
         categoryBtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intentC = new Intent (MainActivity.this,CategoryView.class);
-                intentC.putExtra("co2", c2);
-                flag1 = 2;
-                intentC.putExtra("flag1", flag1);
+                intentC.putExtra("co", categories.get(1));
                 startActivity(intentC);
             }
         });
@@ -91,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // magic button
     public void launchMagicView (View V){
         Intent intentM = new Intent (this,MagicView.class);
         startActivity(intentM);
